a92fa1603829c659f237665a787fa3d1deadcb4c
watual <qordnjsgk123455@gmail.com>
2021-06-23 18:24:26 +0900

    2021.06.23.18-24

--- app/src/main/java/com/example/bibleapp_v1/MessageSender_text.java
+++ app/src/main/java/com/example/bibleapp_v1/MessageSender_text.java
@@ -1,17 +1,51 @@
 package com.example.bibleapp_v1;
 
+import android.content.res.AssetManager;
 import android.os.Bundle;
+import android.os.Environment;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class MessageSender_text extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.message_sender_text);
 
-        File file = new File("/개역개정판성경.txt");
+
+        AssetManager am = getResources().getAssets();
+        InputStream is = null;
+        byte buf[] = new byte[1024];
+        String text = "";
+
+        try {
+            is = am.open("test1.txt");
+            if(is.read(buf) > 0){
+                text = new String(buf);
+            }
+
+            TextView b1 = (TextView)findViewById(R.id.b1);
+            b1.setText(text);
+
+            is.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if(is != null) {
+            try {
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
